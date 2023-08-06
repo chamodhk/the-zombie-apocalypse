9d7e79bb2c8eb70b84ebf073f6ef5a4c97b87a3e
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2023-08-06 15:57:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,81 +13,52 @@ var level = 0;
 
 
 
-function switchVisibility(hideId, showId) {
-    document.getElementById(hideId).style.visibility = "hidden";
-    document.getElementById(showId).style.visibility = "visible";
 
-}
 
-function htp() {
-    switchVisibility("int-rec", "htp-rec");
-    
-}
 
-function htpBack() {
-    switchVisibility("htp-rec", "int-rec");
+function switchVisibility(hideIDs, showIDs) {
+   
+    for (var i = 0; i < hideIDs.length; i++){
+        var obj = document.getElementById(hideIDs[i]);
+        console.log(obj)
+        obj.style.visibility = "hidden"
+    }
 
+    for (var i = 0; i < showIDs.length; i++) {
+        var obj = document.getElementById(showIDs[i]);
+        obj.style.visibility = "visible"
+    }
 }
 
 function charChoose() {
-    switchVisibility("int-rec", "char-rec");
-
+    switchVisibility(['window'], ['char-choose']);
+    console.log("worked")
 }
 
-function charNinja() {
-    switchVisibility("char-rec","bg-rec")
-    character = 1;
+function charSelected(charID) {
+    switchVisibility(["char-choose"], ["bg-choose"])
+    character = charID
 }
 
-function charRHB(){
-    switchVisibility("char-rec", "bg-rec")
-    character = 2;
-}
+function bgSelected(className) {
+    switchVisibility(['bg-choose'], ['main-game']);
 
-function charCG(){
-    switchVisibility("char-rec", "bg-rec")
-
-    character = 3;
-}
-
-function bgV(){
-    document.getElementById("init-game").style.visibility = "hidden";
-    document.getElementById("bg-rec").style.visibility = "hidden";
-    display = document.getElementById("main-game");
-    display.style.visibility = "visible";
-    display.className = "gameBG1";
+    display = document.getElementById('main-game');
+    display.className = className;
     level = 1;
     gameStart();
+
 }
 
-function bgRT(){
-    document.getElementById("init-game").style.visibility = "hidden";
-    document.getElementById("bg-rec").style.visibility = "hidden";
-    display = document.getElementById("main-game");
-    display.style.visibility = "visible";
-    display.className = "gameBG2";
-    level = 2;
-    gameStart();
-}
-
-function bgF(){
-    document.getElementById("init-game").style.visibility = "hidden";
-    document.getElementById("bg-rec").style.visibility = "hidden";
-    display = document.getElementById("main-game");
-    display.style.visibility = "visible";
-    display.className = "gameBG3";
-    level = 3;
-    gameStart();
-}
-
-function keyLog(event){
+function keyLog(event) {
     var x = event.which;
-    if (x == 13){
-
+    console.log(x);
+    if (x == 13) {
+        
     }
-    if (x == 32){
-        if (character == 1){
-            if (jw == 0){
+    if (x == 32) {
+        if (character == 1) {
+            if (jw == 0) {
                 clearInterval(nrw);
                 rs.pause();
                 jw = setInterval(jump, 180);
@@ -95,18 +66,18 @@ function keyLog(event){
                 nrw = -1;
             }
         }
-        if (character == 2){
-            if (jw == 0){
+        if (character == 2) {
+            if (jw == 0) {
                 clearInterval(rhbrw);
                 rs.pause();
                 jw = setInterval(jump, 130);
                 js.play();
                 rhbrw = -1;
             }
-            
+
         }
-        if (character == 3){
-            if (jw == 0){
+        if (character == 3) {
+            if (jw == 0) {
                 clearInterval(cgrw);
                 rs.pause();
                 jw = setInterval(jump, 55);
@@ -117,9 +88,9 @@ function keyLog(event){
     }
 }
 
-function gameStart(){
-    if (character == 1){
-        if (nrw == 0){
+function gameStart() {
+    if (character == 1) {
+        if (nrw == 0) {
             nrw = setInterval(ninjaRun, 100);
             rs.play();
             bgmw = setInterval(bgMove, 100);
@@ -128,8 +99,8 @@ function gameStart(){
             scorew = setInterval(score, 100);
         }
     }
-    if (character == 2){
-        if (rhbrw == 0){
+    if (character == 2) {
+        if (rhbrw == 0) {
             rhbrw = setInterval(RHBRun, 100);
             rs.play();
             bgmw = setInterval(bgMove, 100);
@@ -138,8 +109,8 @@ function gameStart(){
             scorew = setInterval(score, 100);
         }
     }
-    if (character == 3){
-        if (cgrw == 0){
+    if (character == 3) {
+        if (cgrw == 0) {
             cgrw = setInterval(CGRun, 100);
             rs.play();
             bgmw = setInterval(bgMove, 100);
@@ -153,18 +124,18 @@ function gameStart(){
 var genZombieID = 0;
 var p = 1000;
 
-function genZombie(){
-    for (var y = 0; y < 10; y++){
+function genZombie() {
+    for (var y = 0; y < 10; y++) {
         const zombie = document.createElement("img");
         zombie.src = "img/zombie.gif";
         zombie.className = "zombie";
         zombie.style.marginLeft = p + "px";
         zombie.id = "z" + y;
 
-        if(y <= 5){
+        if (y <= 5) {
             p = p + 700;
         }
-        if (y >= 6){
+        if (y >= 6) {
             p = p + 500;
         }
 
@@ -174,17 +145,17 @@ function genZombie(){
 
 var moveZombiew = 0;
 
-function moveZombie(){
-    for (var y = 0; y < 10; y++){
+function moveZombie() {
+    for (var y = 0; y < 10; y++) {
         var z = getComputedStyle(document.getElementById("z" + y));
         var w = parseInt(z.marginLeft) - 25;
         document.getElementById("z" + y).style.marginLeft = w + "px";
         //alert(w);
         //175 - (-125)
-        
-        if (character == 1){
-            if (w >= -125 & w <= 175){
-                if (mt > 500){
+
+        if (character == 1) {
+            if (w >= -125 & w <= 175) {
+                if (mt > 500) {
                     clearInterval(nrw);
                     rs.pause();
                     clearInterval(jw);
@@ -195,11 +166,11 @@ function moveZombie(){
                     dw = setInterval(dead, 100);
                     ds.play();
                 }
-            }   
+            }
         }
-        if (character == 2){
-            if (w >= 0 & w <= 200){
-                if (mt > 500){
+        if (character == 2) {
+            if (w >= 0 & w <= 200) {
+                if (mt > 500) {
                     clearInterval(rhbrw);
                     rs.pause();
                     clearInterval(jw);
@@ -212,9 +183,9 @@ function moveZombie(){
                 }
             }
         }
-        if (character == 3){
-            if (w >= -75 & w <= 175){
-                if (mt > 500){
+        if (character == 3) {
+            if (w >= -75 & w <= 175) {
+                if (mt > 500) {
                     clearInterval(cgrw);
                     rs.pause();
                     clearInterval(jw);
@@ -233,9 +204,9 @@ function moveZombie(){
 var nrw = 0;
 var nr = 0;
 
-function ninjaRun(){
+function ninjaRun() {
     nr = nr + 1;
-    if (nr == 9){
+    if (nr == 9) {
         nr = 1;
     }
     char.src = "img/ninja/Run__00" + nr + ".png";
@@ -244,9 +215,9 @@ function ninjaRun(){
 var rhbrw = 0;
 var rhbr = 1
 
-function RHBRun(){
-    rhbr = rhbr + 1 ;
-    if (rhbr == 9){
+function RHBRun() {
+    rhbr = rhbr + 1;
+    if (rhbr == 9) {
         rhbr = 1;
     }
     char.src = "img/rhb/Run (" + rhbr + ").png";
@@ -255,9 +226,9 @@ function RHBRun(){
 var cgrw = 0;
 var cgr = 1;
 
-function CGRun(){
+function CGRun() {
     cgr = cgr + 1;
-    if (cgr == 21){
+    if (cgr == 21) {
         cgr = 1;
     }
     char.src = "img/cg/Run (" + cgr + ").png";
@@ -268,19 +239,19 @@ var j = 1;
 
 var mt = 550;
 
-function jump(){
+function jump() {
     //alert(mt);
-    if (character == 1){
-        if (j <= 5){
+    if (character == 1) {
+        if (j <= 5) {
             mt = mt - 60;
         }
-        if (j >= 6){
+        if (j >= 6) {
             mt = mt + 60;
         }
         char.style.marginTop = mt + "px";
 
         j = j + 1;
-        if (j == 11){
+        if (j == 11) {
             j = 1;
             clearInterval(jw);
             jw = 0;
@@ -289,17 +260,17 @@ function jump(){
         }
         char.src = "img/ninja/Jump__" + j + ".png";
     }
-    if (character == 2){
-        if (j <= 6){
+    if (character == 2) {
+        if (j <= 6) {
             mt = mt - 50;
         }
-        if (j >= 7){
+        if (j >= 7) {
             mt = mt + 50;
         }
         char.style.marginTop = mt + "px";
 
         j = j + 1;
-        if (j == 13){
+        if (j == 13) {
             j = 1;
             clearInterval(jw);
             jw = 0;
@@ -308,17 +279,17 @@ function jump(){
         }
         char.src = "img/rhb/Jump (" + j + ").png";
     }
-    if (character == 3){
-        if (j <= 15){
+    if (character == 3) {
+        if (j <= 15) {
             mt = mt - 20;
         }
-        if (j >= 16){
+        if (j >= 16) {
             mt = mt + 20;
         }
         char.style.marginTop = mt + "px";
 
         j = j + 1;
-        if (j == 31){
+        if (j == 31) {
             j = 1;
             clearInterval(jw);
             jw = 0;
@@ -329,14 +300,14 @@ function jump(){
     }
 }
 
-function reload(){
+function reload() {
     location.reload();
 }
 
 var b = 0;
 var bgmw = 0;
 
-function bgMove(){
+function bgMove() {
     b = b - 20;
     document.getElementById("main-game").style.backgroundPositionX = b + "px";
 }
@@ -347,7 +318,7 @@ var scorew = 0;
 function score() {
     sc = sc + 5;
     document.getElementById("score").innerHTML = "Score: " + sc;
-    if (sc == 1500){
+    if (sc == 1500) {
         clearInterval(scorew);
         clearInterval(moveZombiew);
         clearInterval(bgmw);
@@ -367,10 +338,10 @@ function score() {
 var d = 0;
 var dw = 0;
 
-function dead(){
-    if (character == 1){
+function dead() {
+    if (character == 1) {
         d = d + 1;
-        if (d == 10){
+        if (d == 10) {
             d = 9;
 
             document.getElementById("main-game").style.visibility = "hidden";
@@ -379,9 +350,9 @@ function dead(){
         }
         char.src = "img/ninja/Dead__00" + d + ".png";
     }
-    if (character == 2){
+    if (character == 2) {
         d = d + 1;
-        if (d == 11){
+        if (d == 11) {
             d = 10;
 
             document.getElementById("main-game").style.visibility = "hidden";
@@ -390,9 +361,9 @@ function dead(){
         }
         char.src = "img/rhb/Dead (" + d + ").png";
     }
-    if (character == 3){
+    if (character == 3) {
         d = d + 1;
-        if (d == 31){
+        if (d == 31) {
             d = 30;
 
             document.getElementById("main-game").style.visibility = "hidden";
@@ -401,5 +372,5 @@ function dead(){
         }
         char.src = "img/cg/Dead (" + d + ").png";
     }
-    
+
 }
